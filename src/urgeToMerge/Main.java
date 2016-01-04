96601b071d06e269e5b9f7cede81aa6b7216bc40
Andy Rock <andyrock1997@gmail.com>
2016-01-03 22:57:34 -0500

    Initial commit

---
+++ src/urgeToMerge/Main.java
@@ -0,0 +1,26 @@
+package urgeToMerge;
+import java.util.Scanner;
+
+/**
+ * Main class of Urge to Merge. This class contains
+ * the main method and is currently being used to
+ * test features of the application.
+ * @author Andy Rock
+ * @version 1.0 January 3, 2016
+**/
+public class Main
+{
+	/**
+	 * Initial testing
+	**/
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Welcome to Urge to Merge!");
+		System.out.println("Hit enter to begin...");
+		in.nextLine();
+
+		in.close();
+	}
+}
