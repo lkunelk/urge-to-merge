0fffa5d74b7c577a328b17a4cff30915ddf34e18
Andy Rock <andyrock1997@gmail.com>
2016-01-03 23:46:32 -0500

    update structure

--- src/urgeToMerge/Main.java
+++ src/urgeToMerge/Main.java
@@ -1,10 +1,12 @@
 package urgeToMerge;
 import java.util.Scanner;
 
+
 /**
  * Main class of Urge to Merge. This class contains
  * the main method and is currently being used to
  * test features of the application.
+ * 
  * @author Andy Rock
  * @version 1.0 January 3, 2016
 **/
@@ -18,7 +20,7 @@ public class Main
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("Welcome to Urge to Merge!");
-		System.out.println("Hit enter to begin...");
+		System.out.print("Hit enter to begin...");
 		in.nextLine();
 
 		in.close();
